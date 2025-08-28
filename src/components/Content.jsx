b7f8d7b30b810c360fb747fbbbac3366dc64769f
sarvesh8939 <ssvvb2004@gmail.com>
2025-08-28 13:07:37 +0530

    code modify002

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,9 +2,7 @@ import React, { useState,useEffect, useContext } from "react";
 import "../components/content.css";
 import Card from "../components/Card.jsx";
 import { AppContext } from "./AppContext";
-// import dotenv from 'dotenv';
 
-// dotenv.config();
 
 function createCard(article, index){
   return (
@@ -24,8 +22,8 @@ function Content() {
   const { selectedValue } = useContext(AppContext);
 
   async function search(selectedValue) {
-    const url = `https://newsapi.org/v2/everything?q=${selectedValue}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`;
-    // process.env.REACT_APP_ANOTHER_KEY
+    const url = `https://newsapi.org/v2/everything?q=${selectedValue}&sortBy=publishedAt&apiKey=0442dbc2127846d78265a93a3edfe509`;
+    // 8c15607f61e64f44b4dcef8b0080b02f
     const response = await fetch(url);
     const data = await response.json();
     setArticles(data.articles);
